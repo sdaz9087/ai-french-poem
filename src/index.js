98d68fb0ee46ec61945792266bd11d8fffa153fe
Sadaf H <145974455+sdaz9087@users.noreply.github.com>
2025-11-25 15:37:46 +0330

    Refactor poem generation functions and update API key

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function generatePoem(event) {
   event.preventDefault();
 
   let instructionsInput = document.querySelector("#user-instructions");
-  let apiKey = "2046c535afeb092fo82f1d306d8a2b2t";
+  let apiKey = "tf34c02900bd1c4o2a013feea2a9c3e5";
   let context =
     "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
   let prompt = `User instructions: Generate a French poem about ${instructionsInput.value}`;
@@ -25,4 +25,4 @@ function generatePoem(event) {
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
-poemFormElement.addEventL
+poemFormElement.addEventListener("submit", generatePoem);
